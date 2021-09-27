a2d4de194473e4f0ba85f9d10d5e1da74fff26cc
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-09-26 22:33:52 -0400

    add to information to local storage

--- assets/Script.js
+++ assets/Script.js
@@ -2,26 +2,23 @@
 let todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-//Store Schedule with click
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
-      let schedule = $(this).siblings(".description").val();
-      let time = $(this).parent().attr("id");
-    });
-
 //Time Loops
  function timeUpdate() {
       let currentTime = moment().hours();
       $(".time-block").each(function() {
         let blockTime = parseInt($(this).attr("id").split("-")[1]);
   // Past/Current/Future Time
+
+        //Past time
         if (blockTime < currentTime) {
           $(this).addClass("past");
         } 
+        //current time
         else if (blockTime === currentTime) {
           $(this).removeClass("past");
           $(this).addClass("present");
         } 
+        //Future time
         else {
           $(this).removeClass("past");
           $(this).removeClass("present");
@@ -32,7 +29,12 @@ $(document).ready(function() {
   
     timeUpdate();
 
-
-
-    
-  });
+    //Save Schedule with click
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+      let schedule = $(this).siblings(".description").val();
+      let time = $(this).parent().attr("id");
+    });
+    localStorage.setItem(schedule, time);
+})
+//store data

a359439196985a4be23632519f6dbb4c14aaaec0
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-09-26 22:26:59 -0400

    updates for present, past, future times

--- assets/Script.js
+++ assets/Script.js
@@ -1,2 +1,38 @@
+//Display Current Date  
 let todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+//Store Schedule with click
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+      let schedule = $(this).siblings(".description").val();
+      let time = $(this).parent().attr("id");
+    });
+
+//Time Loops
+ function timeUpdate() {
+      let currentTime = moment().hours();
+      $(".time-block").each(function() {
+        let blockTime = parseInt($(this).attr("id").split("-")[1]);
+  // Past/Current/Future Time
+        if (blockTime < currentTime) {
+          $(this).addClass("past");
+        } 
+        else if (blockTime === currentTime) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    timeUpdate();
+
+
+
+    
+  });

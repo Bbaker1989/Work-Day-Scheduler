050cb8bab441d44a66e34eaf41f5f495b54074a8
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-09-26 22:43:06 -0400

    corrected storage and hour issue

--- assets/Script.js
+++ assets/Script.js
@@ -34,7 +34,18 @@ $(document).ready(function() {
     $(".saveBtn").on("click", function() {
       let schedule = $(this).siblings(".description").val();
       let time = $(this).parent().attr("id");
-    });
-    localStorage.setItem(schedule, time);
+    
+      localStorage.setItem(time,schedule);
+      console.log(localStorage);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    })
 })
-//store data
